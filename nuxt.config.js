e8bb68f36a4689e2a853e9d8a82f574792cf3f2d
Mauricio Hernan Cabrera <mauriciohernancabrera@gmail.com>
2019-05-17 12:08:02 -0300

    deploy

--- nuxt.config.js
+++ nuxt.config.js
@@ -9,10 +9,10 @@ export default {
   
   mode: 'universal',
 
-  server: {
-    port: 8000, // default: 3000
-    host: '0.0.0.0' // default: localhost
-  },
+  // server: {
+  //   port: 8000, // default: 3000
+  //   host: '0.0.0.0' // default: localhost
+  // },
 
   /*
    ** Headers of the page
